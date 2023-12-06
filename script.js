c7f9554daa7994bdb337ada4277054540cd596a4
Matteo Affatato <matteoaffatato@MacBook-Air-di-Matteo.local>
2023-12-06 16:08:01 +0100

    esercizioJS

---
+++ script.js
@@ -0,0 +1,45 @@
+// Esercizio 1
+
+// alert ('Presentati!');
+// let name = prompt('scrivi il tuo nome');
+// let saluto = 'Ciao a tutti mi chiamo ' + name;
+// alert (saluto);  <-- qui va bene anche console.log (saluto)
+
+// let name = prompt ('Come ti chiami?')
+// let saluto = name + ' sei proprio figo!'
+// console.log (saluto)
+
+
+//  Esercizio 2
+//  let age = prompt ('Quanti anni hai?')
+//  if(age >=18) {
+//      alert ('sei maggiorenne puoi prende la patente')
+//  } else {
+//      alert ('sei minorenne non puoi prendere la patente')
+//  }
+
+// alert ('come preparare il caffè')
+// let step1 = prompt ('step 1 fuoco,caffè,acqua')
+// while(step1 != 'acqua') {
+//     acqua = prompt ('step 1 fuoco,caffè,acqua')
+// }
+// let step2 = prompt ('step 2 fuoco,caffè')
+// while(step2 !='caffè') {
+//     caffè = prompt ('step 2 fuoco,caffè')
+// }
+// let step3 = prompt ('step 3 fuoco')
+// while(step3 !='fuoco') {
+//     fuoco = prompt ('step 3 fuoco')
+// }
+// alert ('bravo sai fare il caffè!')
+
+
+
+
+
+
+
+
+
+
+
